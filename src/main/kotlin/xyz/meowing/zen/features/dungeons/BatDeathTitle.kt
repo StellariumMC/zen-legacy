d404ea20555be179cad81b4aca940d68051ccaca
benji <cg125892@gmail.com>
2025-10-24 17:32:26 +0100

    refactor: migrate bat death detection from packet events to entity events

--- src/main/kotlin/xyz/meowing/zen/features/dungeons/BatDeathTitle.kt
+++ src/main/kotlin/xyz/meowing/zen/features/dungeons/BatDeathTitle.kt
@@ -4,31 +4,30 @@ import xyz.meowing.zen.Zen
 import xyz.meowing.zen.config.ui.ConfigUI
 import xyz.meowing.zen.config.ui.types.ConfigElement
 import xyz.meowing.zen.config.ui.types.ElementType
-import xyz.meowing.zen.events.PacketEvent
 import xyz.meowing.zen.features.Feature
 import xyz.meowing.zen.utils.LocationUtils
 import xyz.meowing.zen.utils.TitleUtils
-import net.minecraft.network.play.server.S29PacketSoundEffect
+import xyz.meowing.zen.events.EntityEvent
+import net.minecraft.entity.item.EntityArmorStand
+import net.minecraft.entity.passive.EntityBat
 
 @Zen.Module
 object BatDeathTitle : Feature("batdeadtitle",true,"catacombs") {
     override fun addConfig(configUI: ConfigUI): ConfigUI {
         return configUI
-            .addElement("Dungeons", "Bat Death Title", ConfigElement(
-                "batdeadtitle",
-                null,
-                ElementType.Switch(false)
-            ), isSectionToggle = true)
+            .addElement(
+                "Dungeons", "Bat Death Title", ConfigElement(
+                    "batdeadtitle",
+                    null,
+                    ElementType.Switch(false)
+                ), isSectionToggle = true
+            )
     }
 
     override fun initialize() {
-        register<PacketEvent.Received> { event ->
-            if (event.packet is S29PacketSoundEffect) {
-                val packet = event.packet
-
-                if (packet.soundName != "mob.bat.death" && packet.soundName != "mob.bat.hurt") return@register
-                if (LocationUtils.subarea?.lowercase()?.contains("boss") == true) return@register
-
+        register<EntityEvent.Leave> {
+            if (it.entity is EntityBat && it.entity.ridingEntity !is EntityArmorStand &&
+                LocationUtils.subarea?.lowercase()?.contains("boss") != true) {
                 TitleUtils.showTitle("§cBat Dead!", null, 1000)
             }
         }
